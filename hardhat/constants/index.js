ca3a586aef346ce27f6d0ee7b0f3a3b0e0770404
dameon green <dameongreen@gmail.com>
2022-07-12 17:17:34 -0400

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x05e60c8d8506CCe1f2a1A961c6CCaC9868420805";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
